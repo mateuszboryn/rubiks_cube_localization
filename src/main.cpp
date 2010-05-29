849db3641061ceb0ed3781e39f4cacefa7aa1996
Mateusz Boryń <mateusz.boryn@gmail.com>
2010-05-30 01:07:49 +0200

    filtracja

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,42 @@
-#include "highgui.h"
+#include <cstdio>
+#include <highgui.h>
 
-int main(int argc, char** argv) {
-	IplImage* img = cvLoadImage(argv[1]);
-	cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
-	cvShowImage( "Example1", img );
+#include "RubiksCubeLocalizator.h"
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		printf("Uzycie: %s <plik z obrazkiem>\n", argv[0]);
+		return 1;
+	}
+
+	char *imageFilename = argv[1];
+
+	printf("Wczytywanie pliku \"%s\"\n", imageFilename);
+	IplImage* original = cvLoadImage(imageFilename);
+	if (original == NULL)
+	{
+		printf("Nie udalo sie wczytac pliku.\n");
+		return 1;
+	}
+	IplImage* afterLocalization = cvCloneImage(original);
+	cvNamedWindow("Oryginalny", CV_WINDOW_AUTOSIZE );
+
+	cvShowImage("Oryginalny", original);
+
+	RubiksCubeLocalizator rkl;
+	if (rkl.locateCube(afterLocalization))
+	{
+		printf("Zlokalizowano kostke.\n");
+		cvNamedWindow("Po lokalizacji");
+		cvShowImage("Po lokalizacji", afterLocalization);
+	}
+
 	cvWaitKey(0);
-	cvReleaseImage( &img );
-	cvDestroyWindow( "Example1" );
+
+	cvReleaseImage(&original);
+	cvReleaseImage(&afterLocalization);
+	cvDestroyWindow("Po lokalizacji");
+	cvDestroyWindow("Oryginalny");
 }
