b857f38fec38baefd98f60ec57d47edbcfbd4545
mateusz <mateusz@mateo.(none)>
2010-05-29 23:49:00 +0200

    kompilacja z opencv

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-#include <cstdio>
+#include "highgui.h"
 
-int main(int argc, char *argv[])
-{
-	printf("POBR - rozpoznawanie kostki Rubika\n");
-	return 0;
+int main(int argc, char** argv) {
+	IplImage* img = cvLoadImage(argv[1]);
+	cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
+	cvShowImage( "Example1", img );
+	cvWaitKey(0);
+	cvReleaseImage( &img );
+	cvDestroyWindow( "Example1" );
 }
