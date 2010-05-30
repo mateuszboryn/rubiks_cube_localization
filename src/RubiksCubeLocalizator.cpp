9e08fc7ebe621ff6305a4a6060851851d8c1a895
Mateusz Boryń <mateusz.boryn@gmail.com>
2010-05-30 12:28:57 +0200

    CMake fixed

--- src/RubiksCubeLocalizator.cpp
+++ src/RubiksCubeLocalizator.cpp
@@ -28,26 +28,60 @@ bool RubiksCubeLocalizator::locateCube(IplImage *image)
 	cvNamedWindow("filteredImage");
 	cvShowImage("filteredImage", filteredImage);
 
-	/*doBinarization(filteredImage);
-	cvNamedWindow("binaryImage");
-	cvShowImage("binaryImage", binaryImage);
+	doBinarization(filteredImage);
+	/*cvNamedWindow("binaryImage");
+	 cvShowImage("binaryImage", binaryImage);
 
-	doBinarization(binaryImage);
-	cvNamedWindow("segmentedImage");
-	cvShowImage("segmentedImage", segmentedImage);*/
+	 doBinarization(binaryImage);
+	 cvNamedWindow("segmentedImage");
+	 cvShowImage("segmentedImage", segmentedImage);*/
 	return false;
 }
 
 void RubiksCubeLocalizator::doFiltering(IplImage *image)
 {
-	filteredImage = cvCreateImage(cvGetSize(image), image->depth, image->nChannels);
+	filteredImage = cvCreateImage(cvGetSize(image), image->depth,
+			image->nChannels);
 	cvSmooth(image, filteredImage, CV_MEDIAN);
 }
 
 void RubiksCubeLocalizator::doBinarization(IplImage *image)
 {
-	/*IplImage* hsv = cvCreateImage(cvGetSize(image), image->depth, image->nChannels);
-	cvCvtColor(image, hsv, CV_BGR2H)*/
+	IplImage* hsv = cvCreateImage(cvGetSize(image), image->depth,
+			image->nChannels);
+	IplImage* h = cvCreateImage(cvGetSize(image), image->depth, 1);
+	IplImage* s = cvCreateImage(cvGetSize(image), image->depth, 1);
+	IplImage* v = cvCreateImage(cvGetSize(image), image->depth, 1);
+	cvCvtColor(image, hsv, CV_BGR2HLS);
+	cvSplit(hsv, h, s, v, NULL);
+
+	cvNamedWindow("h");
+	cvShowImage("h", h);
+
+	cvNamedWindow("s");
+	cvShowImage("s", s);
+
+	cvNamedWindow("v");
+	cvShowImage("v", v);
+
+
+
+	IplImage* YCrCb = cvCreateImage(cvGetSize(image), image->depth,
+				image->nChannels);
+		IplImage* Y = cvCreateImage(cvGetSize(image), image->depth, 1);
+		IplImage* Cr = cvCreateImage(cvGetSize(image), image->depth, 1);
+		IplImage* Cb = cvCreateImage(cvGetSize(image), image->depth, 1);
+		cvCvtColor(image, YCrCb, CV_BGR2YCrCb);
+		cvSplit(YCrCb, Y, Cr, Cb, NULL);
+
+		cvNamedWindow("Y");
+		cvShowImage("Y", Y);
+
+		cvNamedWindow("Cr");
+		cvShowImage("Cr", Cr);
+
+		cvNamedWindow("Cb");
+		cvShowImage("Cb", Cb);
 }
 
 void RubiksCubeLocalizator::doSegmentation(IplImage *image)
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,39 @@
 #include <cstdio>
+#include <iostream>
 #include <highgui.h>
+
+#include <boost/program_options.hpp>
 
 #include "RubiksCubeLocalizator.h"
 
+//using namespace boost::program_options;
+namespace po = boost::program_options;
+using namespace std;
 int main(int argc, char** argv)
 {
+	po::options_description desc("Allowed optionsssssssss");
+	desc.add_options()("help", "produce help message")("compression",
+			po::value<int>(), "set compression level");
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+	if (vm.count("help"))
+	{
+		cout << desc << "\n";
+		return 1;
+	}
+	if (vm.count("compression"))
+	{
+		cout << "Compression level was set to " << vm["compression"].as<int> ()
+				<< ".\n";
+	}
+	else
+	{
+		cout << "Compression level was not set.\n";
+	}
+	return 0;
+
 	if (argc != 2)
 	{
 		printf("Uzycie: %s <plik z obrazkiem>\n", argv[0]);
